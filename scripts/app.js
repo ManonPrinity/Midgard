0c0cd82acc52a3a694b328f8328bde0e306a1c71
Kamipown <paul.delobbel@gmail.com>
2016-07-13 18:24:03 +0200

    First commit

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function($routeProvider)
+{
+	$routeProvider.when("/", {templateUrl: "partials/server.html"});
+	$routeProvider.when("/serveur", {templateUrl: "partials/server.html"});
+	$routeProvider.when("/carte", {templateUrl: "partials/map.html"});
+	$routeProvider.when("/clans", {templateUrl: "partials/clans.html"});
+	$routeProvider.when("/mods", {templateUrl: "partials/mods.html"});
+	// $routeProvider.otherwise({templateUrl: "partials/notfound.html", controller: "notfound_ctrl"});
+});
