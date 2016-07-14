e9ee66167b95fda86251e550c9e1452d39c4c341
Kamipown <paul.delobbel@gmail.com>
2016-07-14 05:12:59 +0200

    map

--- scripts/controllers/map_ctrl.js
+++ scripts/controllers/map_ctrl.js
@@ -11,8 +11,11 @@ app.controller("map_ctrl", function($scope)
 			{
 				var cell = document.createElement("span");
 				cell.className = "cell";
+				cell.classList.add($scope.color_zone(x, y));
+				cell.innerHTML = String.fromCharCode(65 + y) + x;
 				cell.style.width = $scope.map_container.clientWidth / 15 + "px";
 				cell.style.height = $scope.map_container.clientWidth / 15 + "px";
+				cell.style.lineHeight = $scope.map_container.clientWidth / 15 + "px";
 				$scope.map_container.appendChild(cell);
 			}
 		}
@@ -42,5 +45,60 @@ app.controller("map_ctrl", function($scope)
 	$scope.leave = function()
 	{
 		$scope.logs.style.display = "none";
+	}
+
+	$scope.toggle_grid = function()
+	{
+		if ($scope.grid)
+			$scope.map_container.classList.add("grid");
+		else
+			$scope.map_container.classList.remove("grid");
+	}
+
+	$scope.toggle_positions = function()
+	{
+		if ($scope.positions)
+			$scope.map_container.classList.add("positions");
+		else
+			$scope.map_container.classList.remove("positions");
+	}
+
+	$scope.toggle_territories = function()
+	{
+		if ($scope.territories)
+			$scope.map_container.classList.add("territories");
+		else
+			$scope.map_container.classList.remove("territories");
+	}
+
+	$scope.toggle_locked = function()
+	{
+		if ($scope.locked)
+			$scope.map_container.classList.add("locked");
+		else
+			$scope.map_container.classList.remove("locked");
+	}
+
+	$scope.color_zone = function(x, y)
+	{
+		if (y == 0 && x <= 7) return ("NW"); if (y == 0 && x >= 9) return ("NE");
+		if (y == 1 && x <= 7) return ("NW"); if (y == 1 && x >= 9) return ("NE");
+		if (y == 2 && x <= 7) return ("NW"); if (y == 2 && x >= 8) return ("NE");
+		if (y == 3 && x <= 6) return ("NW"); if (y == 3 && x >= 9) return ("NE");
+		if (y == 4 && x <= 6) return ("NW"); if (y == 4 && x >= 9) return ("NE");
+		if (y == 5 && (x <= 4 || x == 6 || x == 7)) return ("NW"); if (y == 5 && x >= 8) return ("NE");
+		if (y == 6 && x <= 2) return ("NW"); if (y == 6 && (x == 10 || x == 11)) return ("NE");
+
+		if (y == 6 && (x == 3 || x == 4 || x == 5)) return ("SW");
+		if (y == 7 && x <= 5) return ("SW");
+		if (y == 8 && x <= 5) return ("SW"); if (y == 8 && x >= 6 && x <= 10) return ("SE");
+		if (y == 9 && x <= 4) return ("SW"); if (y == 9 && x >= 6) return ("SE");
+		if (y == 10 && x <= 5) return ("SW"); if (y == 10 && x >= 6) return ("SE");
+		if (y == 11 && x <= 5) return ("SW"); if (y == 11 && x >= 6) return ("SE");
+		if (y == 12 && x <= 5) return ("SW"); if (y == 12 && x >= 6) return ("SE");
+		if (y == 13 && x <= 5) return ("SW"); if (y == 13 && x >= 6) return ("SE");
+		if (y == 14 && x <= 5) return ("SW"); if (y == 14 && x >= 6) return ("SE");
+
+		return ("locked");
 	}
 });
